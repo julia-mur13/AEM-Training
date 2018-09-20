9d589799cc23505df4523391725090713e7ebf35
julia-mur13 <juliamurashko158@gmail.com>
2018-09-20 09:52:31 +0300

    gulp + webpack + server

--- partials.js
+++ partials.js
@@ -1,1 +1,8 @@
-module.exports.DIR_COMPONENTS = __dirname + '/src/components/';
+const DIR_COMPONENTS = __dirname + '/src/components/';
+
+module.exports.pathsArray = [
+    DIR_COMPONENTS + 'design1.0/Post',
+    DIR_COMPONENTS + 'design1.0/Header',
+    DIR_COMPONENTS + 'design1.0/Footer',
+    DIR_COMPONENTS + 'design1.0/TestComponent',
+];
