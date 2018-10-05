87b7c7bb76cf5576f5ef924493e552c9d8dec219
julia-mur13 <juliamurashko158@gmail.com>
2018-10-05 11:42:31 +0300

    convert from js to ts carousel

--- src/components/bundle-content/bundle.ts
+++ src/components/bundle-content/bundle.ts
@@ -1,31 +1,56 @@
 import welcome from '../design1.0/Header/Header';
+
 welcome('HELLO');
 (function () {
-    function getCurrentSlide(id: string) {
-        const currentCarousel = document.getElementById(`${id}`);
+    function getCurrentSlide(currentCarousel: HTMLDivElement): number {
         const activeDot = currentCarousel.querySelector('.active-dot') as HTMLDivElement;
         return +activeDot.title - 1;
     }
 
-    function goToSlide(currentSlide: number, id: string, nextSlide: number) {
-        const currentCarousel = document.getElementById(`${id}`);
-        const slides = currentCarousel.querySelectorAll('.carousel-items .carousel-item');
-        const navDots = currentCarousel.getElementsByClassName('carousel-dot');
+    function cleanCurrentSlide(currentSlide: number, slides: NodeListOf<HTMLDivElement>, navDots: NodeListOf<HTMLSpanElement>) {
         slides[currentSlide].classList.remove('showing');
         navDots[currentSlide].classList.remove('active-dot');
-        currentSlide = (nextSlide + slides.length) % slides.length;
+    }
+
+    function getNewCurrentSlide(buttonName: string, currentSlide: number, dotNextSlide: string, slides: NodeListOf<HTMLDivElement>): number {
+        let nextSlide: number = 0;
+        switch (buttonName) {
+            case 'PREVIOUS':
+                nextSlide = currentSlide - 1;
+                break;
+            case 'NEXT':
+                nextSlide = currentSlide + 1;
+                break;
+            case 'DOT':
+                if (dotNextSlide) {
+                    nextSlide = +dotNextSlide - 1;
+                }
+                break;
+        }
+        return (nextSlide + slides.length) % slides.length;
+    }
+
+    function showNewCurrentSlide(currentSlide: number, slides: NodeListOf<HTMLDivElement>, navDots: NodeListOf<HTMLSpanElement>) {
         slides[currentSlide].classList.add('showing');
         navDots[currentSlide].classList.add('active-dot');
+    }
+
+    function goToSlide(buttonName: string, event: any) {
+        const target = event.target as HTMLButtonElement;
+        const currentCarousel = target.closest('.carousel-wrapper') as HTMLDivElement;
+        let currentSlide: number = getCurrentSlide(currentCarousel);
+        const slides: NodeListOf<HTMLDivElement> = currentCarousel.querySelectorAll('.carousel-items .carousel-item') as NodeListOf<HTMLDivElement>;
+        const navDots: NodeListOf<HTMLSpanElement> = currentCarousel.getElementsByClassName('carousel-dot')  as NodeListOf<HTMLSpanElement>;
+        cleanCurrentSlide(currentSlide, slides, navDots);
+        currentSlide = getNewCurrentSlide(buttonName, currentSlide, target.title, slides);
+        showNewCurrentSlide(currentSlide, slides, navDots);
     }
 
     function clickPreviousSlide() {
         const previousArrows = document.getElementsByClassName('arrow-previous-btn');
         for (let index = 0; index < previousArrows.length; index++) {
             previousArrows[index].addEventListener('click', (event) => {
-                const target = event.target as HTMLButtonElement;
-                const carouselId = target.closest('.carousel-wrapper').id;
-                const currentSlide = getCurrentSlide(carouselId);
-                goToSlide(currentSlide, carouselId, currentSlide - 1);
+                goToSlide('PREVIOUS', event);
             });
         }
     }
@@ -34,10 +59,7 @@ welcome('HELLO');
         const nextArrows = document.getElementsByClassName('arrow-next-btn');
         for (let index = 0; index < nextArrows.length; index++) {
             nextArrows[index].addEventListener('click', (event) => {
-                const target = event.target as HTMLButtonElement;
-                const carouselId = target.closest('.carousel-wrapper').id;
-                const currentSlide = getCurrentSlide(carouselId);
-                goToSlide(currentSlide, carouselId, currentSlide + 1);
+                goToSlide('NEXT', event);
             });
         }
     }
@@ -46,12 +68,7 @@ welcome('HELLO');
         const dotsArray = document.getElementsByClassName('carousel-dots-wrapper');
         for (let index = 0; index < dotsArray.length; index++) {
             dotsArray[index].addEventListener('click', (event) => {
-                const target = event.target as HTMLSpanElement;
-                if(target.title) {
-                    const carouselId = target.closest('.carousel-wrapper').id;
-                    const currentSlide = getCurrentSlide(carouselId);
-                    goToSlide(currentSlide, carouselId, +target.title - 1);
-                }
+                goToSlide('DOT', event);
             });
         }
     }
