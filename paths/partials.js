74c4cbd2b358d1fdc2a281f147663b590d2d5442
julia-mur13 <juliamurashko158@gmail.com>
2018-10-22 13:42:25 +0300

    -move core directory

--- paths/partials.js
+++ paths/partials.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
 const DIR_COMPONENTS = path.join(__dirname, '../src/components/');
+const DIR_CORE = path.join(__dirname, '../src/core/');
 const DIR_TEST_PAGES = path.join(__dirname, '../test-pages/pages/');
 
 module.exports.pathsArray = [
     DIR_COMPONENTS + 'MainPage/Post',
-    DIR_COMPONENTS + 'core/Header',
-    DIR_COMPONENTS + 'core/Footer',
+    DIR_CORE + 'Header',
+    DIR_CORE + 'Footer',
     DIR_COMPONENTS + 'MainPage/SlideCarousel',
     DIR_COMPONENTS + 'MainPage/Main',
     DIR_COMPONENTS + 'FullPost',
--- src/components/bundle-content/bundle.ts
+++ src/components/bundle-content/bundle.ts
@@ -1,4 +1,4 @@
-import SlideCarousel from '../core/SlideCarousel/SlideCarousel';
-import SlideCarouselDots from '../core/SlideCarousel/SlideCarouselDots';
+import SlideCarousel from '../../core/SlideCarousel/SlideCarousel';
+import SlideCarouselDots from '../../core/SlideCarousel/SlideCarouselDots';
 
 console.log(SlideCarousel, SlideCarouselDots);
